0fdebec92c2544603cd55b113d2d5c5daed6cbed
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-03-21 12:39:17 +0100

    ui in progress

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh p-6">
+    <div className="flex h-dvh w-dvw">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
           <h1 className="font-blog-title text-4xl text-primary/80">Project ready!</h1>
@@ -12,6 +12,9 @@ export default function Page() {
         </div>
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
+        </div>
+        <div className="flex justify-center w-200 h-800 border-2 border-red-500">
+          <Button>Hello</Button>
         </div>
       </div>
     </div>
