b235fec0dd5ae65020d232245c0c53a825cf94a0
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-03-27 08:03:45 +0100

    Wrap SocialItem component in SocialItemMotion for enhanced animations

--- features/profile/components/social-item.tsx
+++ features/profile/components/social-item.tsx
@@ -1,6 +1,7 @@
 import type { SocialIcon } from "@/components/icons/social-icons";
 import { GlowBorder } from "@/components/custom/glow-border";
 import { Item, ItemContent, ItemMedia, ItemTitle } from "@/components/ui/item";
+import { SocialItemMotion } from "@/features/profile/components/social-item-motion";
 
 type Social = {
   link?: string;
@@ -10,15 +11,17 @@ type Social = {
 
 function SocialItem({ label, Icon }: Social) {
   return (
-    <Item variant="outline" className="relative">
-      <GlowBorder />
-      <ItemMedia className="relative z-10">
-        <Icon className="size-10 text-black dark:text-white" />
-      </ItemMedia>
-      <ItemContent className="relative z-10">
-        <ItemTitle>{label}</ItemTitle>
-      </ItemContent>
-    </Item>
+    <SocialItemMotion>
+      <Item variant="outline" className="relative">
+        <GlowBorder />
+        <ItemMedia className="relative z-10">
+          <Icon className="size-10 text-black dark:text-white" />
+        </ItemMedia>
+        <ItemContent className="relative z-10">
+          <ItemTitle>{label}</ItemTitle>
+        </ItemContent>
+      </Item>
+    </SocialItemMotion>
   );
 }
 
