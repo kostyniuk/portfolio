a65ef4cde58265b1ef1d352f7c214c9a62bb9e82
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-03-30 20:41:21 +0200

    Enhance Avatar component and Interests display
    - Introduced variant support for the Avatar component using class-variance-authority.
    - Refactored Interests component to dynamically render a list of interests with tooltips, improving maintainability and visual consistency.
    - Updated Avatar usage in Interests to support new variant and className properties.

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -4,19 +4,34 @@ import * as React from "react";
 import { Avatar as AvatarPrimitive } from "@base-ui/react/avatar";
 
 import { cn } from "@/lib/utils";
+import { VariantProps, cva } from "class-variance-authority";
+
+const avatarVariants = cva("", {
+  variants: {
+    variant: {
+      default: "",
+      ghost: "after:hidden bg-transparent",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
 
 function Avatar({
   className,
   size = "default",
+  variant = "default",
   ...props
 }: AvatarPrimitive.Root.Props & {
   size?: "default" | "sm" | "lg";
-}) {
+} & VariantProps<typeof avatarVariants>) {
   return (
     <AvatarPrimitive.Root
       data-slot="avatar"
       data-size={size}
       className={cn(
+        avatarVariants({ variant }),
         "group/avatar relative flex size-8 shrink-0 rounded-full select-none after:absolute after:inset-0 after:rounded-full after:border after:border-border after:mix-blend-darken data-[size=lg]:size-10 data-[size=sm]:size-6 dark:after:mix-blend-lighten",
         className,
       )}
--- features/interests/components/interests.tsx
+++ features/interests/components/interests.tsx
@@ -1,26 +1,87 @@
 "use client";
 
-import { Avatar, AvatarFallback, AvatarGroup, AvatarImage } from "@/components/ui/avatar";
+import { ReactDark } from "@/components/icons/interests-icons";
+import { Avatar, AvatarGroup, AvatarImage } from "@/components/ui/avatar";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { ReactElement } from "react";
+
+type Interest = {
+  name: string;
+  src: string;
+  fallback?: string;
+  className?: string;
+  Icon?: ReactElement;
+};
+
+const INTERESTS: Interest[] = [
+  {
+    name: "shadcn",
+    src: "https://github.com/shadcn.png",
+    className: "after:hidden bg-transparent",
+  },
+  {
+    name: "React",
+    src: "https://react.dev/apple-touch-icon.png",
+    Icon: <ReactDark />,
+  },
+  {
+    name: "Node.js",
+    src: "https://github.com/nodejs.png",
+  },
+  {
+    name: "TypeScript",
+    src: "https://www.typescriptlang.org/icons/icon-512x512.png",
+  },
+  {
+    name: "PostgreSQL",
+    src: "https://www.postgresql.org/media/img/about/press/elephant.png",
+  },
+  {
+    name: "Next.js",
+    src: "https://assets.vercel.com/image/upload/v1662130559/nextjs/Icon_dark_background.png",
+  },
+  {
+    name: "Bun",
+    src: "https://bun.sh/logo-square.png",
+  },
+  {
+    name: "Vercel",
+    src: "https://github.com/vercel.png",
+  },
+  {
+    name: "TanStack",
+    src: "https://github.com/TanStack.png",
+  },
+  {
+    name: "Drizzle",
+    src: "https://github.com/drizzle-team.png",
+  },
+  {
+    name: "MySQL",
+    src: "https://www.mysql.com/common/logos/logo-mysql-170x115.png",
+  },
+];
 
 function Interests() {
   return (
     <div>
       <h1>Interests</h1>
       <AvatarGroup className="gap-4">
-        <Tooltip>
-          <TooltipTrigger>
-            <Avatar size="lg">
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </TooltipTrigger>
-          <TooltipContent>shadcn</TooltipContent>
-        </Tooltip>
-        <Avatar size="lg">
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        {INTERESTS.map(({ name, className, src, Icon }) => (
+          <Tooltip key={name}>
+            <TooltipTrigger>
+              <Avatar size="lg" variant="ghost" className={className}>
+                {!Icon ? (
+                  <AvatarImage src={src} alt={name} />
+                ) : (
+                  <div className="size-full overflow-hidden rounded-full [&>svg]:size-full [&>svg]:block">{Icon}</div>
+                )}
+                {/* <AvatarFallback>{interest.fallbac ?? interest.name.slice(0, 2).toUpperCase()}</AvatarFallback> */}
+              </Avatar>
+            </TooltipTrigger>
+            <TooltipContent>{name}</TooltipContent>
+          </Tooltip>
+        ))}
       </AvatarGroup>
     </div>
   );
