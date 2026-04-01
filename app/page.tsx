3b7f4193697eb371222408d7b9dbee704e2092d9
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-04-01 21:08:10 +0200

    Refactor Page layout for improved structure
    - Updated the Page component to enhance layout consistency by removing unnecessary classes.
    - Simplified the structure of the main container for better readability and maintainability.

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ import { Experience } from "@/features/experience/components/experience";
 
 export default function Page() {
   return (
-    <div className="flex justify-center ">
-      <div className="flex flex-col gap-8 min-h-dvh w-1/2 bg-foreground/2 text-sm leading-loose p-8 max-w-172 overflow-scroll">
+    <div className="flex justify-center min-h-dvh">
+      <div className="flex flex-col gap-8 w-1/2 bg-foreground/2 text-sm leading-loose p-8 max-w-172 ">
         <div className="font-mono text-xs text-muted-foreground text-center">
           (Press <Kbd className="mx-1">d</Kbd> to toggle dark mode)
         </div>
