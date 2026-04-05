b132b4ef6d5075a5b97d9e3929be727651c2404c
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-04-05 12:34:09 +0200

    Update social media links in Profile component
    - Changed the link and label for the X social network from "@costiniuc00" to "@kostyniuk00" and updated the corresponding URL to reflect the new username.
    - Ensured consistency across social media profiles in the Profile component.

--- features/profile/components/socials.tsx
+++ features/profile/components/socials.tsx
@@ -2,7 +2,7 @@ import { GitHubIcon, InstagramIcon, LinkedInIcon, XIcon } from "@/components/ico
 import { LinkItem, type LinkItemProps } from "@/features/profile/components/link-item";
 
 const SOCIAL_NETWORKS = {
-  x: { link: "https://x.com/costiniuc00", label: "@costiniuc00", Icon: XIcon },
+  x: { link: "https://x.com/kostyniuk00", label: "@kostyniuk00", Icon: XIcon },
   github: { link: "https://github.com/kostyniuk", label: "@kostyniuk", Icon: GitHubIcon },
   instagram: { link: "https://www.instagram.com/costiniuc00", label: "@costiniuc00", Icon: InstagramIcon },
   linkedin: { link: "https://www.linkedin.com/in/kostyniuk", label: "@kostyniuk", Icon: LinkedInIcon },
