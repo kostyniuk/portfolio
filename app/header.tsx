4af3f5272f8675b0fcd4d4bf8c6a83a74a04400f
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-04-09 19:26:37 +0200

    Add Octokit integration to Header component for GitHub repository data

--- app/header.tsx
+++ app/header.tsx
@@ -3,8 +3,18 @@ import { FrostGlass } from "@/components/ui/frost-glass";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { SocialItemMotion } from "@/features/profile/components/social-item-motion";
 import Link from "next/link";
+import { Octokit } from "octokit";
+import { ItemDescription } from "@/components/ui/item";
+import { Button } from "@/components/ui/button";
 
-function Header() {
+async function Header() {
+  const octokit = new Octokit();
+
+  const { data: repo } = await octokit.rest.repos.get({
+    owner: "kostyniuk",
+    repo: "portfolio",
+  });
+
   return (
     <div className="fixed z-1 w-full flex justify-center">
       <div className="mt-2 w-[calc(100%-2rem)] sm:w-[calc(50%-10px)] rounded-4xl overflow-hidden">
@@ -13,7 +23,10 @@ function Header() {
             <ThemeToggle />
             <Link href="https://github.com/kostyniuk/portfolio">
               <SocialItemMotion>
-                <GitHubIcon className="size-6 text-black dark:text-white" />
+                <Button variant={"ghost"} className="flex flex-row gap-2 items-center cursor-pointer">
+                  <GitHubIcon className="size-6 text-black dark:text-white" />
+                  <ItemDescription>{repo.stargazers_count}</ItemDescription>
+                </Button>
               </SocialItemMotion>
             </Link>
           </div>
