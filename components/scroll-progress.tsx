f8635683085f761d547cdf84327ed68d7da7593e
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-04-12 20:27:13 +0200

    Remove debug log from ScrollProgress component

--- components/scroll-progress.tsx
+++ components/scroll-progress.tsx
@@ -5,8 +5,6 @@ import { motion, useScroll } from "motion/react";
 function ScrollProgress() {
   const { scrollYProgress } = useScroll();
 
-  console.log({ scrollYProgress });
-
   return (
     <motion.div
       className="fixed h-2 top-0 left-0 bg-foreground w-full origin-left z-2"
