5731954e61dcb0fdcb6913717f565d51e92692c4
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-05-23 19:08:54 +0200

    re-design. remove what not need anymore

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Socials } from "@/features/profile/components/socials";
 import { Interests } from "@/features/interests/components/interests";
 import { Education } from "@/features/education/components/education";
 import { Experience } from "@/features/experience/components/experience";
-import { AppleHelloEffectEnglish } from "@/components/apple-hello-effect/apple-hello-effect-english";
+import { AppleWelcomeEffectEnglish } from "@/components/apple-hello-effect/apple-welcome-effect-english";
 import { Contributions } from "@/features/contributions/contributions";
 import { Preference } from "@/features/preferences/components/preference";
 
@@ -13,10 +13,7 @@ export default function Page() {
   return (
     <>
       <div className="mt-8 flex flex-col items-center text-center font-mono text-xs gap-4 sm:mt-0">
-        <div className="flex text-center items-baseline text-muted-foreground">
-          (Press <Kbd className="mx-1">d</Kbd> to toggle dark mode)
-        </div>
-        <AppleHelloEffectEnglish className={"text-foreground"} loop loopDelay={2000} />
+        {/* <AppleWelcomeEffectEnglish className={"text-foreground"} loop loopDelay={2000} /> */}
       </div>
       <Bio />
       <Socials />
