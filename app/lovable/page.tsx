ce46e7d4fd582a808fa3caafbde9ba4d1683b508
Alex Kostyniuk <kostyniuk7@gmail.com>
2026-06-21 00:46:31 +0200

    Add Lovable intro copy and portrait

--- app/lovable/page.tsx
+++ app/lovable/page.tsx
@@ -30,6 +30,17 @@ export default function LovablePage() {
           style={{ filter: "blur(4px)" }}
         />
       </div>
+      <h2 className="absolute bottom-[calc(50%_+_13.5rem)] left-1/2 w-[min(90vw,48rem)] -translate-x-1/2 text-center text-3xl font-semibold tracking-normal text-white sm:text-4xl lg:text-5xl">
+        Hey Lovable team, I&apos;m Alex
+      </h2>
+      <img
+        src="/lovable/me.png"
+        alt="Alex"
+        className="absolute h-100 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 object-contain"
+      />
+      <h2 className="absolute left-1/2 top-[calc(50%_+_13.5rem)] w-[min(90vw,48rem)] -translate-x-1/2 text-center text-2xl font-semibold tracking-normal text-white sm:text-4xl lg:text-5xl">
+        Scroll down to see the reasons why we&apos;re a perfect fit
+      </h2>
     </div>
   );
 }
